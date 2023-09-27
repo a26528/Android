870ebac0726b621fa60b6dc8ad6c26a6190c5505
a26528 <145553495+a26528@users.noreply.github.com>
2023-09-27 12:14:08 +0200

    app android
    mira el de alberto para el archivo java

--- 1AppAndroid/app/src/main/java/com/example/a1_appandroid/MainActivity.java
+++ 1AppAndroid/app/src/main/java/com/example/a1_appandroid/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.a1_appandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtUser;
+    private EditText edtPassword;
+    private Button btnSignin;
 
+    /*CICLO DE VIDA: ONLOAD JAVASCRIPT*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //document.getElementById("idUser");
+         edtUser = findViewById(R.id.edtUser);
+         edtPassword = findViewById(R.id.edtPassword);
+         btnSignin = findViewById(R.id.btnSignin);
+
+         //btnEnviar.addEventListener("click","Bienvenido a JS");
+        
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
