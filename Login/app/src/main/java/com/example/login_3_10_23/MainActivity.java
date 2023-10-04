8b58dade01212db1b7805931ea29b2f12ef0082d
LeyreDiaz <a26528@svalero.com>
2023-10-04 11:45:19 +0200

    poco java

--- Login/app/src/main/java/com/example/login_3_10_23/MainActivity.java
+++ Login/app/src/main/java/com/example/login_3_10_23/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.login_3_10_23;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtEmail;
+    private EditText edtPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponents();
     }
+
+    private void initComponents() {
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin= findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+
+
 }
