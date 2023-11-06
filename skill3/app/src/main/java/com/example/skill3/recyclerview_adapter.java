49b4e7bdbdfc023d9d35e27ede82d4cf140080e0
LeyreDiaz <a26528@svalero.com>
2023-11-06 11:24:42 +0100

    skill3 10:58

---
+++ skill3/app/src/main/java/com/example/skill3/recyclerview_adapter.java
@@ -0,0 +1,53 @@
+package com.example.skill3;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class recyclerview_adapter extends RecyclerView.Adapter<recyclerview_adapter.ViewHolder> {
+
+   private ArrayList<recyclerview_list> recyclerview_list;
+   private Context context;
+
+    public recyclerview_adapter(ArrayList<com.example.skill3.recyclerview_list> recyclerview_list, Context context) {
+        this.recyclerview_list = recyclerview_list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_card,parent,false);
+       return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull recyclerview_adapter.ViewHolder holder, int position) {
+        holder.imageView.setImageResource((recyclerview_list.get(position).getImage()));
+        holder.textView.setText(recyclerview_list.get(position).getText());
+    }
+
+    @Override
+    public int getItemCount() {
+        return recyclerview_list.size();
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView imageView;
+        TextView textView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+            textView = itemView.findViewById(R.id.textView);
+        }
+    }
+}
