b7352b22aabbcc59f795c5841f60ffe54523af45
LeyreDiaz <a26528@svalero.com>
2023-11-20 09:12:24 +0100

    si

--- LoginAndroid_29_09_2023/app/src/main/java/com/example/loginandroid_29_09_2023/login_user/ContractLoginUser.java
+++ LoginAndroid_29_09_2023/app/src/main/java/com/example/loginandroid_29_09_2023/login_user/ContractLoginUser.java
@@ -4,13 +4,13 @@ import com.example.loginandroid_29_09_2023.beans.User;
 
 public interface ContractLoginUser {
     public interface View{
-        public void successLogin(User user); //me viene del BACK
-        void failureLogin(String err);      //me viene del BACK
+        public void successLogin(User user);
+        void failureLogin(String err);
         // void failureLogin(MyException err);
     }
     public interface Presenter{
         // void login(String email, String pass);
-        void login(User user); //ida hacia el back
+        void login(User user);
         // void login(ViewUser viewUser);
         // VIEW-ORM
         // BEANS-ENTITIES
@@ -18,10 +18,10 @@ public interface ContractLoginUser {
     }
     public interface Model{
         interface OnLoginUserListener{
-            void onFinished(User user); //vuelta de back
+            void onFinished(User user);
             void onFailure(String err);
         }
         void loginAPI(User user,
-                      OnLoginUserListener onLoginUserListener); //va hacia el back
+                      OnLoginUserListener onLoginUserListener);
     }
 }
